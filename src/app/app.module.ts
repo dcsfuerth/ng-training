4dd51dbc7a5dda792143fe5378bc60e83833ac03
Peter Dickten <peter.dickten@dcs-fuerth.de>
2023-09-12 16:04:37 +0200

    WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BookListComponent } from './books/book-list/book-list.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, BookListComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/books/book-list/book-list.component.ts
+++ src/app/books/book-list/book-list.component.ts
@@ -6,18 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./book-list.component.css'],
 })
 export class BookListComponent {
-  public books = [
+  coverIsVisible = true;
+
+  books = [
     {
       isbn: '978-1-891830-77-8',
       title: 'The Lord of the TS',
+      coverUrl:
+        'https://m.media-amazon.com/images/I/71Wv+d6oP6L._AC_UY218_.jpg',
+      price: 9,
     },
     {
       isbn: '978-1-891830-77-9',
       title: 'The Lord of the JS',
+      coverUrl:
+        'https://m.media-amazon.com/images/I/71uSIJwhnmL._AC_UY218_.jpg',
+      price: 19,
     },
   ];
 
-  // books: { isbn: string; title: string }[] = [];
+  breite = 100 * Math.random();
+
+  toggleCover() {
+    this.coverIsVisible = !this.coverIsVisible;
+  }
 
   getBuchId(_index: number, book: any) {
     return book.isbn;
