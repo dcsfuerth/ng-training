ceb3f94b99fcd5fec7fc124a519439569115d7c6
Peter Dickten <peter.dickten@dcs-fuerth.de>
2023-09-13 10:33:53 +0200

    WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,16 @@ import { BookListComponent } from './books/book-list/book-list.component';
 import { FormsModule } from '@angular/forms';
 import { CalculatorComponent } from './shared/calculator/calculator.component';
 
+import localeDe from '@angular/common/locales/de';
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeDe);
+
 @NgModule({
   declarations: [AppComponent, BookListComponent, CalculatorComponent],
   imports: [BrowserModule, FormsModule],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: 'de' }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
